10982fd99c8822abd1deeead3a9769c82032f926
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2021-10-13 16:52:23 +0100

    Implement: `utils.named`

--- case/utils.py
+++ case/utils.py
@@ -1,12 +1,13 @@
 import io
 import typing
 import types
-# import itertools
 
-def compose(*functions: typing.Callable):
-    def wrapper(*args, **kwargs):
+def compose(*functions: typing.Callable) -> typing.Callable:
+    def wrapper(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
         response: typing.Any = None
 
+        index: int
+        function: typing.Callable
         for index, function in enumerate(functions):
             response = \
             (
@@ -25,8 +26,8 @@ def identity(x: typing.Any) -> typing.Any:
 def is_lambda(obj: typing.Any) -> bool:
     return isinstance(obj, types.LambdaType) and obj.__name__ == '<lambda>'
 
-def chunk(string: str, sizes):
-    buffer = io.StringIO(string)
+def chunk(string: str, sizes: typing.List[int]) -> typing.List[str]:
+    buffer: io.StringIO = io.StringIO(string)
 
     return \
     [
@@ -34,5 +35,5 @@ def chunk(string: str, sizes):
         for size in sizes
     ]
 
-def concatenate(strings):
-    return ''.join(strings)
+def named(obj: typing.Any) -> bool:
+    return hasattr(obj, '__name__')
